4d5b45db9566bc95908a3e267fb0868b155c9d39
fathyaa12 <alfathya@students.amikom.ac.id>
2026-04-17 14:54:43 +0700

    Tugas 2

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,34 @@ Route::get('/katalog', function () {
     return view('katalog');
 });
 
+Route::get('/bantuan', function () {
+    return view('bantuan');
+});
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/admin/event', function () {
+    return view('admin.event');
+});
+
+Route::get('/admin/transactions', function () {
+    return view('admin.transactions');
+});
+
+Route::get('/ticket', function () {
+    return view('layouts.ticket');
+});
+
+Route::get('/event-detail', function () {
+    return view('layouts.event-detail');
+});
+
+Route::get('/chekout', function () {
+    return view('layouts.chekout');
 });
