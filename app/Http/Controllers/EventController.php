e432ca6724ba1006af35ccee55a095c9d0a0d21a
fathyaa12 <alfathya@students.amikom.ac.id>
2026-04-17 14:54:25 +0700

    Tugas 2

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index(){
+        return view("event-detail");
+    }
+
+    public function show(){
+        return view("chekout");
+    }
+}
